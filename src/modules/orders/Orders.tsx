2bd83e7999c3f9fa787a5723272bdb771faf9737
antonturch <antonturch@mail.ru>
2022-04-25 17:55:26 +0300

    Refactor orders component

--- src/modules/orders/Orders.tsx
+++ src/modules/orders/Orders.tsx
@@ -1,32 +1,35 @@
 import React, { FC, useContext, useEffect } from "react";
 import { observer } from "mobx-react-lite";
 import { Context } from "../../index";
+import OrderItem from "./orderItem";
 import "./index.scss";
 
 const Orders: FC = () => {
   const { store } = useContext(Context);
+  let orders = store.orders;
 
   useEffect(() => {
     store.getOrders();
   }, []);
 
   return (
-    <div className="orders-wrapper">
-      <h1>Orders</h1>
+    <div className="orders">
       {store.isAuth ? (
-        <div>
-          <h1>
+        <>
+          <h2 className="orders__title">
             Hi <span>{store.user.firstName} </span>
             <span>{store.user.lastName}!</span>
-          </h1>
-          Here will be shown your orders
-        </div>
+          </h2>
+          <p>Your orders list{store.orders.length ? ":" : " is empty"}</p>
+          <ul>
+            {orders.map((el) => (
+              <OrderItem order={el} key={el.orderId} />
+            ))}
+          </ul>
+        </>
       ) : (
-        <div>Please authorize to see your order list</div>
+        <p>Please authorize to see your order list</p>
       )}
-      <div>
-        {store.orders.length}
-      </div>
     </div>
   );
 };
